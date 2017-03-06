ffd4aa2ae15407ae9bcbb417e2d91f9f914a2313
Michael Pavlov <mihpavlov@gmail.com>
2017-03-06 14:15:39 +0300

    Basic templates, timezone settings

--- live_magazine/settings.py
+++ live_magazine/settings.py
@@ -100,9 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Simferopol'
 
 USE_I18N = True
 
