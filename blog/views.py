9ef41171c34e7fcbdda826bf9b72130703a4579b
Michael Pavlov <mihpavlov@gmail.com>
2017-03-06 14:56:28 +0300

    implements UserPostsView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
 from django.views.generic import DetailView
@@ -6,6 +7,8 @@ from django.views.generic import UpdateView
 
 from blog.forms import PostForm
 from blog.models import Post
+
+User = get_user_model()
 
 
 class FeedView(ListView):
@@ -19,9 +22,15 @@ class FeedView(ListView):
 class UserPostsView(ListView):
     model = Post
 
+    def get_context_data(self, **kwargs):
+        context = super(UserPostsView, self).get_context_data(**kwargs)
+        username = self.kwargs.get('username')
+        context['author'] = User.objects.get(username=username)
+        return context
+
     def get_queryset(self):
-        profile = self.request.user.profile
-        return Post.objects.filter(profile=profile)
+        username = self.kwargs.get('username')
+        return Post.objects.filter(profile__user__username=username)
 
 
 class CreatePostView(CreateView):
