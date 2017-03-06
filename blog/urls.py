ef00995feb5b52ec8d70dc44bfb42efa4623626d
Michael Pavlov <mihpavlov@gmail.com>
2017-03-06 12:23:14 +0300

    Reorganize views

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,13 @@
 from django.conf.urls import url
 
-from blog.views import FeedView, UserPostsView, SubscribeToUserView, CreatePostView, DetailPostView, EditPostView, \
-    DeletePostView, MarkPostAsReadView
+from blog.views import FeedView, UserPostsView, CreatePostView, DetailPostView, EditPostView, DeletePostView
 
 urlpatterns = [
     url(r'^feed$', FeedView.as_view(), name='feed'),
-    url(r'^posts$', UserPostsView.as_view(), name='posts'),
-    url(r'^posts/subscribe', SubscribeToUserView.as_view(), name='subscribe'),
+    url(r'^posts$', UserPostsView.as_view(), name='posts'),  # subscribe тут же в метод post
     url(r'^posts/create$', CreatePostView.as_view(), name='create'),
-    url(r'^posts/(?P<pk>\d+)', DetailPostView.as_view(), name='detail'),
+    url(r'^posts/(?P<pk>\d+)', DetailPostView.as_view(), name='detail'),  # mark_as_read тут же в метод post
     url(r'^posts/edit', EditPostView.as_view(), name='edit'),
     url(r'^posts/delete', DeletePostView.as_view(), name='delete'),
-    url(r'^posts/(?P<pk>\d+)/read', MarkPostAsReadView.as_view(), name='mark-read'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import DetailView
 from django.views.generic import ListView
+from django.views.generic import UpdateView
 
 
 class FeedView(ListView):
+    pass
+
+
+class UserPostsView(ListView):
+    pass
+
+
+class CreatePostView(CreateView):
+    pass
+
+
+class DetailPostView(DetailView):
+    pass
+
+
+class EditPostView(UpdateView):
+    pass
+
+
+class DeletePostView(DeleteView):
     pass
