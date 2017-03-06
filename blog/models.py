0715e7e9a42405f68e08df0febe004280aa9d410
Michael Pavlov <mihpavlov@gmail.com>
2017-03-06 12:31:40 +0300

    Adds post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.conf import settings
+from django.db.models import ForeignKey
+from django.db.models import Model, DateTimeField, CharField, TextField
 
-# Create your models here.
+
+class Post(Model):
+    timestamp = DateTimeField(auto_now=False, auto_now_add=True)
+    title = CharField(max_length=250)
+    content = TextField()
+    user = ForeignKey(settings.AUTH_USER_MODEL, default=1)
+
+    def __str__(self):
+        return self.title
